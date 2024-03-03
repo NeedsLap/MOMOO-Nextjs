46db1bbda384c0b19a51d1c77d15700e6a04dd9b
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-03-04 02:02:20 +0900

    :bug: Fix: 빌드 시 버그로 인한 임시 주석 처리

--- src/components/Upload/UploadImageToStorage.tsx
+++ src/components/Upload/UploadImageToStorage.tsx
@@ -1,59 +1,59 @@
-import {
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
-  getStorage,
-} from 'firebase/storage';
+// import {
+//   ref,
+//   uploadBytesResumable,
+//   getDownloadURL,
+//   getStorage,
+// } from 'firebase/storage';
 
-const uploadImageToStorage = async (
-  files: FileList,
-  folderName: string,
-  feedId: string,
-) => {
-  const storage = getStorage();
-  const downloadURLs: string[] = [...files].map(() => '');
-  const uploadPromises = Array.from(files).map((file, i) => {
-    const storageRef = ref(storage, `${folderName}/${feedId}${i}`);
-    const uploadTask = uploadBytesResumable(storageRef, file);
+// const uploadImageToStorage = async (
+//   files: FileList,
+//   folderName: string,
+//   feedId: string,
+// ) => {
+//   const storage = getStorage();
+//   const downloadURLs: string[] = [...files].map(() => '');
+//   const uploadPromises = Array.from(files).map((file, i) => {
+//     const storageRef = ref(storage, `${folderName}/${feedId}${i}`);
+//     const uploadTask = uploadBytesResumable(storageRef, file);
 
-    return new Promise((resolve, reject) => {
-      uploadTask.on(
-        'state_changed',
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log('Upload is ' + progress + '% done');
-          switch (snapshot.state) {
-            case 'paused':
-              console.log('Upload is paused');
-              break;
-            case 'running':
-              console.log('Upload is running');
-              break;
-          }
-        },
-        (error) => {
-          console.error('이미지 업로드 오류:', error);
-          reject(error);
-        },
-        async () => {
-          try {
-            const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
-            downloadURLs[i] = downloadURL;
+//     return new Promise((resolve, reject) => {
+//       uploadTask.on(
+//         'state_changed',
+//         (snapshot) => {
+//           const progress =
+//             (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+//           console.log('Upload is ' + progress + '% done');
+//           switch (snapshot.state) {
+//             case 'paused':
+//               console.log('Upload is paused');
+//               break;
+//             case 'running':
+//               console.log('Upload is running');
+//               break;
+//           }
+//         },
+//         (error) => {
+//           console.error('이미지 업로드 오류:', error);
+//           reject(error);
+//         },
+//         async () => {
+//           try {
+//             const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+//             downloadURLs[i] = downloadURL;
 
-            resolve(undefined);
-          } catch (error) {
-            console.error('다운로드 URL 가져오기 오류:', error);
-            reject(error);
-          }
-        },
-      );
-    });
-  });
+//             resolve(undefined);
+//           } catch (error) {
+//             console.error('다운로드 URL 가져오기 오류:', error);
+//             reject(error);
+//           }
+//         },
+//       );
+//     });
+//   });
 
-  await Promise.all(uploadPromises);
+//   await Promise.all(uploadPromises);
 
-  return downloadURLs;
-};
+//   return downloadURLs;
+// };
 
-export default uploadImageToStorage;
+// export default uploadImageToStorage;
--- src/hooks/useEditFeed.ts
+++ src/hooks/useEditFeed.ts
@@ -1,31 +1,31 @@
-import { doc, updateDoc } from 'firebase/firestore';
+// import { doc, updateDoc } from 'firebase/firestore';
 
-import { appFireStore } from '@/firebase/config';
-import useAuthContext from '@/hooks/useAuthContext';
-import useEditContext from '@/hooks/useEditContext';
+// import { appFireStore } from '@/firebase/config';
+// import useAuthState from '@/hooks/auth/useAuthState';
+// import useEditContext from '@/hooks/useEditContext';
 
-export default function useEditFeed() {
-  const { user } = useAuthContext();
-  const { feedIdToEdit } = useEditContext();
+// export default function useEditFeed() {
+//   const { user } = useAuthState();
+//   const { feedIdToEdit } = useEditContext();
 
-  const editFeed = async (updateData: {}, id?: string) => {
-    if (user === null) {
-      return;
-    }
+//   const editFeed = async (updateData: {}, id?: string) => {
+//     if (user === null) {
+//       return;
+//     }
 
-    let feedDocRef;
+//     let feedDocRef;
 
-    if (feedIdToEdit) {
-      feedDocRef = doc(appFireStore, user.uid, user.uid, 'feed', feedIdToEdit);
-    } else if (id) {
-      feedDocRef = doc(appFireStore, user.uid, user.uid, 'feed', id);
-    } else {
-      console.error('id 아규먼트가 누락되었습니다.');
-      return;
-    }
+//     if (feedIdToEdit) {
+//       feedDocRef = doc(appFireStore, user.uid, user.uid, 'feed', feedIdToEdit);
+//     } else if (id) {
+//       feedDocRef = doc(appFireStore, user.uid, user.uid, 'feed', id);
+//     } else {
+//       console.error('id 아규먼트가 누락되었습니다.');
+//       return;
+//     }
 
-    await updateDoc(feedDocRef, updateData);
-  };
+//     await updateDoc(feedDocRef, updateData);
+//   };
 
-  return editFeed;
-}
+//   return editFeed;
+// }
