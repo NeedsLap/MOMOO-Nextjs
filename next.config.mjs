2bc1e9d1d352af0517497a59658e01e655efa9c8
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-03-03 16:19:44 +0900

    :bulb: Feat: ssr - styled-component 적용(#1)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
+};
 
 export default nextConfig;
