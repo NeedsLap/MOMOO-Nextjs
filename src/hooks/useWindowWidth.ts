a8ee37fd58de5f97d955c2a250c9cc5b0ec4d7b6
Kim Hayeon <108985221+KimHayeon1@users.noreply.github.com>
2024-03-03 23:28:09 +0900

    :bug: Fix: clientWidth -> windowWidth(스크롤 포함)/window 예외 처리(#1)

---
+++ src/hooks/useWindowWidth.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+
+export default function useWindowWidth() {
+  const [windowWidth, setWindowWidth] = useState<null | number>(null);
+
+  useEffect(() => {
+    const changeWindowWidth = () => {
+      setWindowWidth(window ? window.innerWidth : 0);
+    };
+
+    changeWindowWidth();
+    window.addEventListener('resize', changeWindowWidth);
+
+    return window.removeEventListener('resize', changeWindowWidth);
+  }, []);
+
+  return windowWidth;
+}
