18faf6eb6723aa4493dc48c269a0fe4b37c7df66
kimhayeon <k.hayeon00@gmail.com>
2024-03-08 22:38:16 +0900

    :lipstick: Style: h1, MOMOO로 변경

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -3,26 +3,25 @@ import { useRouter } from 'next/navigation';
 
 import StyledTopbar from '@/components/Topbar/StyledTopbar';
 
-const TopBar = ({ tit }: { tit: string }) => {
+export default function TopBar({ tit }: { tit: string }) {
   const router = useRouter();
 
-  const handleBtn = () => {
+  const goBack = () => {
     router.back();
   };
 
   return (
     <StyledTopbar>
-      <h1>{tit}</h1>
+      <h1 className="a11y-hidden">MOMOO</h1>
+      <h2>{tit}</h2>
       <button
         type="button"
         className="back"
-        onClick={handleBtn}
+        onClick={goBack}
         aria-label="이전 페이지로 가기"
       >
-        <Image src="/icons/srrow-back.svg" alt="" width={16} height={16} />
+        <Image src="/icons/arrow-back.svg" alt="" width={16} height={16} />
       </button>
     </StyledTopbar>
   );
-};
-
-export default TopBar;
+}
