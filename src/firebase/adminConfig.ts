5c5299f7ffa7ad9e0c1006ca96afdb832770b1ff
kimhayeon <k.hayeon00@gmail.com>
2024-03-11 21:39:21 +0900

    :bug: Fix: 환경변수 - 개행 문자 버그 수정

--- src/firebase/adminConfig.ts
+++ src/firebase/adminConfig.ts
@@ -4,7 +4,7 @@ const adminApp = admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
     clientEmail: process.env.CLIENT_EMAIL,
-    privateKey: process.env.PRIVATEKEY,
+    privateKey: (process.env.PRIVATE_KEY as string).replace(/\\n/g, '\n'),
   }),
 });
 
