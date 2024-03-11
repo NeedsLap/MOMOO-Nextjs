894c7e4672b699f7540b7ab72be49e3d07758c92
kimhayeon <k.hayeon00@gmail.com>
2024-03-11 15:35:38 +0900

    :recycle: Refactor: My 페이지 추가(#1)

---
+++ src/app/my/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next';
+
+import My from '@/containers/my/My';
+import { getProfile } from '@/utils/admin';
+
+export const metadata: Metadata = {
+  title: 'My | MOMOO',
+};
+
+export default async function page() {
+  const profile = await getProfile();
+
+  return <My profile={profile} />;
+}
