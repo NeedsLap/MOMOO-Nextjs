8ec4364ff942f081718d928b71040da77944febf
kimhayeon <k.hayeon00@gmail.com>
2024-03-18 14:23:25 +0900

    :lipstick: Style: 이전 커밋으로 되돌리기

--- src/firebase/adminConfig.ts
+++ src/firebase/adminConfig.ts
@@ -1,9 +1,11 @@
 import admin from 'firebase-admin';
 
 const adminApp = admin.initializeApp({
-  credential: admin.credential.cert(
-    process.env.GOOGLE_APPLICATION_CREDENTIALS || '',
-  ),
+  credential: admin.credential.cert({
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    clientEmail: process.env.CLIENT_EMAIL,
+    privateKey: (process.env.PRIVATE_KEY as string).replace(/\\n/g, '\n'),
+  }),
 });
 
 const adminAppAuth = adminApp.auth();
