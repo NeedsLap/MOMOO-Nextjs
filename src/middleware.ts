91e7f9e12408623d8a12e2804431586677cf8223
kimhayeon <k.hayeon00@gmail.com>
2024-03-21 22:19:54 +0900

    :bug: Fix: middleware - matcher 동작하지 않는 버그

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,29 +2,29 @@ import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-const nonAuthPaths = ['/login', '/signup'];
-const authPaths = [
-  '/',
-  '/:uid/:albumName',
-  '/:uid/:albumName/feed',
-  '/my',
-  '/upload',
-  '/edit-feed', // 경로 확정 전
-  '/edit-profile',
-];
-
 export function middleware(request: NextRequest) {
   const hasCookie = cookies().has('uid');
+  const nonAuthPaths = ['/login', '/signup'];
 
   if (hasCookie && nonAuthPaths.includes(request.nextUrl.pathname)) {
-    return NextResponse.redirect(new URL('/', request.url));
+    return NextResponse.redirect(new URL('/login', request.url));
   }
 
   if (!hasCookie && !nonAuthPaths.includes(request.nextUrl.pathname)) {
-    return NextResponse.redirect(new URL('/login', request.url));
+    return NextResponse.redirect(new URL('/', request.url));
   }
 }
 
 export const config = {
-  matcher: [...nonAuthPaths, authPaths],
+  matcher: [
+    '/login',
+    '/signup',
+    '/',
+    '/:uid/:albumName',
+    '/:uid/:albumName/feed',
+    '/my',
+    '/upload',
+    '/edit-feed', // 경로 확정 전
+    '/edit-profile',
+  ],
 };
