f4d7b035711447655c27a7bded1fb67c0e304d7f
yonainthefish <yonaisgood@gmail.com>
2024-04-02 21:07:23 +0900

    :wastebasket: Remove: 사용하지 않는 context 파일 살제

--- src/hooks/useUploadContext.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-
-import { UploadContext } from '@/context/UploadContext';
-
-export default function useUploadContext() {
-  const context = useContext(UploadContext);
-
-  return context;
-}
