1cf1405fdadde7e125cd876a29e2c92a0290029b
kimhayeon <k.hayeon00@gmail.com>
2024-04-03 14:56:20 +0900

    :bug: Fix: api url 환경변수로 변경

--- src/services/feed.ts
+++ src/services/feed.ts
@@ -1,6 +1,6 @@
-import type { GetFeedsQuery, GetFeedsOpts } from '@/services/model';
+import { API_URL } from '@/services/constant';
 
-const url = 'http://localhost:3000/api';
+import type { GetFeedsQuery, GetFeedsOpts } from '@/services/model';
 
 const getFeeds = async ({
   limit,
@@ -11,6 +11,7 @@ const getFeeds = async ({
 }: GetFeedsQuery) => {
   const opts: GetFeedsOpts = {
     method: 'GET',
+    // cache: 'no-store',
   };
 
   if (cookie) {
@@ -19,7 +20,7 @@ const getFeeds = async ({
     };
   }
   const res = await fetch(
-    `${url}/feed?limit=${limit}&skip=${skip}&album=${albumName}&uid=${uid}`,
+    `${API_URL}/feed?limit=${limit}&skip=${skip}&album=${albumName}&uid=${uid}`,
     opts,
   );
 
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,13 +1,13 @@
-const url = 'http://localhost:3000/api';
+import { API_URL } from '@/services/constant';
 
 const getUserByEmail = async (email: string) => {
-  const res = await fetch(`${url}/user?email=${email}`);
+  const res = await fetch(`${API_URL}/user?email=${email}`);
 
   return res;
 };
 
 const deleteUser = async () => {
-  const res = await fetch(`${url}/user`, {
+  const res = await fetch(`${API_URL}/user`, {
     method: 'DELETE',
   });
 
