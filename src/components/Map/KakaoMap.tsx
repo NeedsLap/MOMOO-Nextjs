0c9195000c5e7a0d93721235546d7d286c612ab0
yonainthefish <yonaisgood@gmail.com>
2024-04-03 17:11:55 +0900

    :recycle: Refactor: 카카오맵 리팩토링(#21)

--- src/components/Map/KakaoMap.tsx
+++ src/components/Map/KakaoMap.tsx
@@ -1,103 +1,112 @@
-// import Image from 'next/image';
-// import React, { useState } from 'react';
+import Image from 'next/image';
+import React, { useState } from 'react';
 
-// import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
-// import { MapContainer, SearchHead } from '@/components/Map/StyledKakaoMap';
+import { MapContainer, SearchHead } from '@/components/Map/StyledKakaoMap';
 
-// function KakaoMap({
-//   onAddressSelect,
-// }: {
-//   onAddressSelect: (address: string) => void;
-// }) {
-//   const [state, setState] = useState({
-//     center: { lat: 37.49676871972202, lng: 127.02474726969814 },
-//     isPanto: true,
-//   });
+interface Place {
+  y: string;
+  x: string;
+  place_name: string;
+}
 
-//   const [searchQuery, setSearchQuery] = useState('');
-//   const [searchedLocation, setSearchedLocation] = useState<{
-//     place_name: string | null;
-//   } | null>(null);
+function KakaoMap({
+  onAddressSelect,
+}: {
+  onAddressSelect: (address: string) => void;
+}) {
+  const [state, setState] = useState({
+    center: { lat: 37.49676871972202, lng: 127.02474726969814 },
+    isPanto: true,
+  });
 
-//   async function handleSearch() {
-//     if (searchQuery) {
-//       const ps = new window.kakao.maps.services.Places();
-//       ps.keywordSearch(searchQuery, (data, status) => {
-//         if (status === window.kakao.maps.services.Status.OK) {
-//           handlePlacesSearch(data);
-//         }
-//       });
-//     }
-//   }
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchedLocation, setSearchedLocation] = useState<{
+    place_name: string | null;
+  } | null>(null);
 
-//   const handlePlacesSearch = (data: any[]) => {
-//     if (data && data.length > 0) {
-//       const newSearch = data[0];
-//       const newPosition = { lat: newSearch.y, lng: newSearch.x };
-//       setState((prevState) => ({
-//         ...prevState,
-//         center: newPosition,
-//       }));
-//       setSearchedLocation(newSearch);
-//       console.log('검색된 위치 정보:', newPosition);
-//     }
-//   };
+  async function handleSearch() {
+    if (searchQuery) {
+      const ps = new window.kakao.maps.services.Places();
+      ps.keywordSearch(searchQuery, (data, status) => {
+        if (status === window.kakao.maps.services.Status.OK) {
+          handlePlacesSearch(data);
+        }
+      });
+    }
+  }
 
-//   const handleSearchQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     setSearchQuery(e.target.value);
-//   };
+  const handlePlacesSearch = (data: Place[]) => {
+    if (data && data.length > 0) {
+      const newSearch = data[0];
+      const newPosition = {
+        lat: parseFloat(newSearch.y),
+        lng: parseFloat(newSearch.x),
+      };
+      setState((prevState) => ({
+        ...prevState,
+        center: newPosition,
+      }));
+      setSearchedLocation(newSearch);
+      console.log('검색된 위치 정보:', newPosition);
+    }
+  };
 
-//   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-//     if (e.key === 'Enter') {
-//       handleSearch();
-//     }
-//   };
+  const handleSearchQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
 
-//   return (
-//     <MapContainer>
-//       <SearchHead>
-//         <input
-//           onChange={handleSearchQueryChange}
-//           onKeyDown={handleKeyDown}
-//           value={searchQuery}
-//           placeholder="주소 또는 키워드 입력해 주세요"
-//         />
-//         {searchedLocation && <div></div>}
-//         <button
-//           type="button"
-//           className="saveBtn"
-//           aria-label="Select address"
-//           onClick={() =>
-//             onAddressSelect(
-//               searchedLocation?.place_name ? searchedLocation.place_name : '',
-//             )
-//           }
-//         >
-//           <Image
-//             src="/icons/select.svg"
-//             alt="선택 버튼"
-//             className="btnUpload"
-//             width={24}
-//             height={24}
-//           />
-//         </button>
-//       </SearchHead>
-//       <div className="mapBoxWrapper">
-//         <Map
-//           center={state.center}
-//           isPanto={state.isPanto}
-//           style={{
-//             width: '100%',
-//             height: '100%',
-//           }}
-//           level={3}
-//         >
-//           <MapMarker position={state.center}></MapMarker>
-//         </Map>
-//       </div>
-//     </MapContainer>
-//   );
-// }
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
 
-// export default KakaoMap;
+  return (
+    <MapContainer>
+      <SearchHead>
+        <input
+          onChange={handleSearchQueryChange}
+          onKeyDown={handleKeyDown}
+          value={searchQuery}
+          placeholder="주소 또는 키워드 입력해 주세요"
+        />
+        {searchedLocation && <div></div>}
+        <button
+          type="button"
+          className="saveBtn"
+          aria-label="Select address"
+          onClick={() =>
+            onAddressSelect(
+              searchedLocation?.place_name ? searchedLocation.place_name : '',
+            )
+          }
+        >
+          <Image
+            src="/icons/select.svg"
+            alt="선택 버튼"
+            className="btnUpload"
+            width={24}
+            height={24}
+          />
+        </button>
+      </SearchHead>
+      <div className="mapBoxWrapper">
+        <Map
+          center={state.center}
+          isPanto={state.isPanto}
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+          level={3}
+        >
+          <MapMarker position={state.center}></MapMarker>
+        </Map>
+      </div>
+    </MapContainer>
+  );
+}
+
+export default KakaoMap;
