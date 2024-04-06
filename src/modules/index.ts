5f5679abf4409151fbf9a4fee05a1fdd74061258
kimhayeon <k.hayeon00@gmail.com>
2024-04-06 12:06:49 +0900

    :recycle: Refactor: editFeedModal 전역 상태 제거

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,7 +1,6 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import authReducer from '@/modules/auth';
-import editFeedModalReducer from '@/modules/editFeedModal';
 import pageReducer from '@/modules/page';
 import signupReducer from '@/modules/signup';
 import uploadFeedModalReducer from '@/modules/uploadFeedModal';
@@ -10,7 +9,6 @@ const rootReducer = combineReducers({
   auth: authReducer,
   page: pageReducer,
   signup: signupReducer,
-  editFeedModal: editFeedModalReducer,
   uploadFeedModal: uploadFeedModalReducer,
 });
 
