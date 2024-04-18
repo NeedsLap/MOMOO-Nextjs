4230841754dd6bae30d5bb13b038797a7d9394e4
yonainthefish <yonaisgood@gmail.com>
2024-04-18 20:30:29 +0900

    :bug: Fix: 이미지 불러올 때 모든 파일의 확장자 검사하도록 수정(#49)

--- src/components/FileUpload/Preview.tsx
+++ src/components/FileUpload/Preview.tsx
@@ -18,22 +18,32 @@ const Preview = ({
     const files = e.target.files;
 
     if (files && files.length > 0) {
-      const file = files[0];
-      console.log(file.type);
-      if (
-        !/^image\/(jpg|svg(\+xml)?|png|jpeg|gif|bmp|tif|heic)$/.test(file.type)
-      ) {
+      let valid = true;
+
+      for (const file of files) {
+        if (
+          !/^image\/(jpg|svg(\+xml)?|png|jpeg|gif|bmp|tif|heic)$/.test(
+            file.type,
+          )
+        ) {
+          valid = false;
+          break;
+        }
+
+        if (file.size > 10 * 1024 * 1024) {
+          valid = false;
+          break;
+        }
+      }
+
+      if (!valid) {
         setSubmitErrMessage(
           '이미지 파일 확장자는 jpg, svg, png, jpeg, gif, bmp, tif, heic만 가능합니다.',
         );
         return;
       }
 
-      if (file.size > 10 * 1024 * 1024) {
-        setSubmitErrMessage('이미지 용량은 10MB 이내로 등록 가능합니다.');
-        return;
-      }
-
+      // 모든 파일이 유효한 경우 이미지 설정 및 업로드
       setImages(files);
       setFile(files);
     } else {
@@ -48,10 +58,12 @@ const Preview = ({
         const newImages: string[] = [];
 
         for (const file of fileArray) {
+          // 파일을 URL로 변환하여 이미지 리스트에 추가
           const imageUrl = URL.createObjectURL(file);
           newImages.push(imageUrl);
         }
 
+        // 이미지 리스트 업데이트
         setImageList(newImages);
       } else {
         setSubmitErrMessage('최대 10장의 이미지까지만 선택할 수 있습니다.');
@@ -60,6 +72,7 @@ const Preview = ({
   };
 
   const handleRemoveImage = (indexToRemove: number) => {
+    // 이미지 리스트에서 이미지 제거
     setImageList((currentImgList) =>
       currentImgList.filter((_, index) => index !== indexToRemove),
     );
