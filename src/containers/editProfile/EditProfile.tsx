284529868fb7bfd59d66b1d3e708ce18915c955c
kimhayeon <k.hayeon00@gmail.com>
2024-04-25 21:04:42 +0900

    :bulb: Feat: input - accept 추기

--- src/containers/editProfile/EditProfile.tsx
+++ src/containers/editProfile/EditProfile.tsx
@@ -301,6 +301,7 @@ export default function EditProfile({ profile }: EditProfileProps) {
             </label>
             <input
               id="profile-inp"
+              accept="image/*"
               type="file"
               className="a11y-hidden"
               onClick={(e) =>
