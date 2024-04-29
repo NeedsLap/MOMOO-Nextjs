59571af03c7a8eae147f470fd8431c1948ff35ee
kimhayeon <k.hayeon00@gmail.com>
2024-04-29 22:05:04 +0900

    :art: Design: upload(pc, tablet) - 닫기 아이콘 white로 변경

--- src/components/Upload/UploadModal/UploadModal.tsx
+++ src/components/Upload/UploadModal/UploadModal.tsx
@@ -287,14 +287,26 @@ export default function UploadModal() {
           <Styled.CloseBtn
             className="closeBtn"
             onClick={() => closeUploadModal()}
+            aria-label="닫기"
           >
-            <Image
-              className={kakaoMapVisible ? 'rotate' : ''}
-              src="/icons/x-white.svg"
-              width={24}
-              height={24}
-              alt={'닫기'}
-            ></Image>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M20.4001 3.60059L3.60012 20.4006"
+                stroke="white"
+                strokeWidth="2"
+              />
+              <path
+                d="M20.4001 20.3999L3.60012 3.59987"
+                stroke="white"
+                strokeWidth="2"
+              />
+            </svg>
           </Styled.CloseBtn>
         </Styled.ContentContainer>
       </Styled.StyledDialog>
