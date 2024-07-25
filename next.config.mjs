22c1541c626dac7e7654111185ea672ace209856
kimhayeon <k.hayeon00@gmail.com>
2024-07-25 16:19:58 +0900

    :teddy_bear: Chore: svg 파일 - React 컴포넌트 변환 설정(#155)

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,14 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     domains: ['firebasestorage.googleapis.com'],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
