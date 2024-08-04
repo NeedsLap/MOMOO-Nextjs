dd1ab3ba1770a77ebfc249485d78ba9840fa713a
kimhayeon <k.hayeon00@gmail.com>
2024-08-04 13:31:10 +0900

    :recycle: Refactor: svg - 컴포넌트로 불러오기(#155)

---
+++ src/assets/Svgs.ts
@@ -0,0 +1,5 @@
+import SplashLogoCircle from 'public/icons/splash-logo-circle.svg';
+import SplashLogo from 'public/icons/splash-logo.svg';
+import XWhite from 'public/icons/x-white.svg';
+
+export { SplashLogo, SplashLogoCircle, XWhite };
