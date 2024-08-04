5df0d43264a886b5e734a6cb97a0686730ff55b0
kimhayeon <k.hayeon00@gmail.com>
2024-08-04 13:36:32 +0900

    :lipstick: Style: 매개변수명 변경 및 airbnb 컨벤션 맞추기(#158)

--- src/hooks/useUpdateFeedList.ts
+++ src/hooks/useUpdateFeedList.ts
@@ -6,20 +6,11 @@ import useAuthState from '@/hooks/auth/useAuthState';
 export function useRemoveFeedIdFromFeedList() {
   const { user } = useAuthState();
 
-  const removeFeedIdFromFeedList = async (
-    feedId: string,
-    unSelectedAlbumId: string,
-  ) => {
-    const albumRefQuery = doc(
-      appFireStore,
-      user.uid,
-      user.uid,
-      'album',
-      unSelectedAlbumId,
-    );
+  const removeFeedIdFromFeedList = async (feedId: string, albumId: string) => {
+    const albumRefQuery = doc(appFireStore, user.uid, user.uid, 'album', albumId);
 
     await updateDoc(albumRefQuery, {
-      feedList: arrayRemove(feedId),
+      feedList: arrayRemove(feedId)
     });
   };
 
@@ -29,20 +20,11 @@ export function useRemoveFeedIdFromFeedList() {
 export function useAddFeedIdFromFeedList() {
   const { user } = useAuthState();
 
-  const addFeedIdFromFeedList = async (
-    feedId: string,
-    selectedAlbumId: string,
-  ) => {
-    const albumRefQuery = doc(
-      appFireStore,
-      user.uid,
-      user.uid,
-      'album',
-      selectedAlbumId,
-    );
+  const addFeedIdFromFeedList = async (feedId: string, selectedAlbumId: string) => {
+    const albumRefQuery = doc(appFireStore, user.uid, user.uid, 'album', selectedAlbumId);
 
     await updateDoc(albumRefQuery, {
-      feedList: arrayUnion(feedId),
+      feedList: arrayUnion(feedId)
     });
   };
 
