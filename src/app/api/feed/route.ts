607ffc85f9bbf3214eeac13d1aaadcf8572c71e6
kimhayeon <k.hayeon00@gmail.com>
2024-09-26 19:39:03 +0900

    :bug: Fix: GET feed - 예외 처리

--- src/app/api/feed/route.ts
+++ src/app/api/feed/route.ts
@@ -5,7 +5,7 @@ import {
   checkAlbumPermission,
   getAlbumByName,
   getFeedsData,
-  getSharedAlbums,
+  getSharedAlbums
 } from '@/utils/SDKUtils';
 
 import { AlbumType } from '@/types/album';
@@ -20,36 +20,36 @@ export async function GET(req: NextRequest) {
   if (!userUid) {
     return NextResponse.json(
       {
-        error: '인증되지 않은 사용자입니다.',
+        error: '인증되지 않은 사용자입니다.'
       },
       {
-        status: 401,
-      },
+        status: 401
+      }
     );
   }
 
   if (!limit || !skip || !albumName || !uid) {
     return NextResponse.json(
       {
-        error: '요청 매개변수가 누락되었습니다.',
+        error: '요청 매개변수가 누락되었습니다.'
       },
       {
-        status: 400,
-      },
+        status: 400
+      }
     );
   }
 
-  const limitNum = parseInt(limit);
-  const skipNum = parseInt(skip);
+  const limitNum = parseInt(limit, 10);
+  const skipNum = parseInt(skip, 10);
 
   if (limitNum <= skipNum || skipNum < 0) {
     return NextResponse.json(
       {
-        error: '요청 매개변수가 올바르지 않습니다.',
+        error: '요청 매개변수가 올바르지 않습니다.'
       },
       {
-        status: 400,
-      },
+        status: 400
+      }
     );
   }
 
@@ -58,11 +58,11 @@ export async function GET(req: NextRequest) {
   if (!albumDoc) {
     return NextResponse.json(
       {
-        error: '존재하지 않는 앨범입니다.',
+        error: '존재하지 않는 앨범입니다.'
       },
       {
-        status: 404,
-      },
+        status: 404
+      }
     );
   }
 
@@ -76,23 +76,27 @@ export async function GET(req: NextRequest) {
   if (!hasPermission) {
     return NextResponse.json(
       {
-        error: '접근 권한이 없는 앨범입니다.',
+        error: '접근 권한이 없는 앨범입니다.'
       },
       {
-        status: 403,
-      },
+        status: 403
+      }
     );
   }
 
   // 최신순으로 가져오기 위해 뒤에서부터 slice
   const feedList: string[] = [...albumDoc.data().feedList];
+
+  if (feedList.length < skipNum) {
+    return NextResponse.json([]);
+  }
+
   const albumType: AlbumType = userUid === uid ? 'my' : 'shared';
-  const startIndex =
-    feedList.length - limitNum < 0 ? 0 : feedList.length - limitNum;
+  const startIndex = feedList.length - limitNum < 0 ? 0 : feedList.length - limitNum;
   const feeds = await getFeedsData(
     feedList.slice(startIndex, feedList.length - skipNum).reverse(),
     uid,
-    albumType,
+    albumType
   );
 
   return NextResponse.json(feeds);
